5733be87103577dcac9cd343c6d76cd446bc3420
zendabor <yumedzi@gmail.com>
2021-12-01 22:17:36 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import '/src/style.css';
+import img from '/src/assets/1024px-Unofficial_JavaScript_logo_2.svg.png';
+
+
+const jsImg = document.createElement('img');
+jsImg.className = 'img__js';
+jsImg.src = img;
+document.body.append(jsImg);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/script.js',
+    devServer: {
+        static: './dist',
+        port: 9090,
+        open: true,
+    },
+    output: {
+        filename: 'script.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                }, ],
+            }
+        ],
+
+    }
+}
